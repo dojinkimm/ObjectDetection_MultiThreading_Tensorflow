32cd81485829be26e22b71f11cfbe1479299c339
Henry Kim <dojinkim119@gmail.com>
2019-07-29 15:57:45 +0900

    multithreading pretrained & yolo

--- README.md
+++ README.md
@@ -1,1 +1,143 @@
-# Faster_Object_Detection
+# Object Detection with MultiThreading using Tensorflow 
+**한글**로 된 README를 보기 위해서는 해당 링크로 가면 됩니다 - [README-kr](/readme/README-kr.md)
+
+This project is a simple opencv, tensorflow, pytorch implementation of **Faster RCNN**, **Mask RCNN**, **YOLO**. 
+The purpose of this project is to implement a simple object detection program using various frameworks.
+Object detection programs were tested on CPU environment as well as on GPU(NVIDIA GeForce GTX 1080).
+However, since opencv does not support cuda operations for NVIDIA gpu, opencv DNN codes were tested only on CPU.
+There **isn't a code for training**, only codes to detect objects with pre-trained models. THe program goes through following processes:
+
+1. Reads video file or launches web camera
+2. Detects objects with pre-trained models (Trained on **COCO**)
+3. Draws bounding boxes and labels.
+
+While implementing, I referred to amazing implementations of other repositories listed below: 
+
+* Opencv Dnn, Pytorch FasterRCNN <br/>
+https://github.com/spmallick/learnopencv
+* Pytorch-Yolo <br/>
+https://github.com/ayooshkathuria/pytorch-yolo-v3
+* Tensorflow, Tensorflow-Yolo <br/>
+https://github.com/tensorflow/models/tree/master/research/object_detection<br/> 
+https://github.com/wizyoung/YOLOv3_TensorFlow<br/>
+
+
+## Requirements
+
+* Python 3.6
+* torch 1.0<br>
+```pip install torch==1.0```
+* torchvision 0.3.0
+* opencv-python 4.1.0
+* imutils 0.5.2<br> 
+```pip install torchvision opencv-python imutils```
+* tensorflow 1.14.0
+* tensorflow-gpu 1.1.0<br>
+```pip install tensorflow tensorflow-gpu```
+
+
+##How To Get Pre-trained Models
+In the project pre-trained models were obtained from 3 sources. First, from tensorflow object detection api site.
+Second, from yolo site. Third, pre-trained model that is automatically downloaded by pytorch framework. 
+### 1. Tensorflow Object Detection API
+
+1.1 Go to Tensorflow Object Detection API page
+<br/>
+https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md
+
+<img src="readme/tensorflow_api.png" width="600px"/>
+
+1.2 Find table that looks like the image and download COCO pre-trained model.
+
+### 2. Yolo
+1.1 Go to Yolo page.<br/>
+https://pjreddie.com/darknet/yolo/
+
+1.2 Download pre-trained model, place files under `darknet` directory(The project was tested only on Yolov3)
+
+### 3. Pytorch automatic pre-trained model
+From `torchvision 0.3.0` pre-trained model for object detection is provided. The framework automatically downloads `pth` file.
+```python
+from torchvision import models
+
+model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+```
+
+## How To Convert Pre-trained models to use in other frameworks.
+
+### 1. Tensorflow Pre-trained model => Opencv Dnn
+In order to use pre-trained model in **Opencv**, frozen inference graph file `.pb` must be converted to `.pbtxt` file.
+The way to convert `.pb` file differs by object detection architecture <br/>
+Example) if downloaded pre-trained model is faster rcnn
+```Shell
+$ python tf_text_graph_faster_rcnn.py --input /path/to/.pb --output /path/to/.pbtxt --config /path/to/pipeline.config
+``` 
+
+### 2. Yolo Pre-trained model => Tensorflow
+In order to use yolo in tensorflow, the `.weights` file must be converted to `ckpt` file. 
+First, make sure that `yolo_anchors.txt` file is under `darknet` directory.
+Second, place downloaded `yolov3.weights` file and `yolov3.cfg` file under `darknet` directory<br/>
+```Shell
+python convert_weight.py
+``` 
+After the command, `ckpt` file and other necessary files for yolo object detection will be placed under `darknet` directory.
+
+## Running Demos
+Assuming that video file is under `assets` directory and yolo `weights, ckpt, cfg` files under `darknet` directory.
+Replace `/path/to/...` part with actual path to files.
+#### FasterRCNN Opencv
+```
+python opencv_fasterrcnn.py --video assets/cars.mp4 --pbtxt /path/to/.pbtxt --frozen /path/to/frozen_inference_graph.pb --conf 0.5
+```
+
+#### MaskRCNN Opencv
+```Shell
+python opencv_maskrcnn.py --video assets/cars.mp4 --pbtxt /path/to/.pbtxt --frozen /path/to/frozen_inference_graph.pb --conf 0.5 --mask 0.3
+```
+
+#### Yolo Opencv
+```Shell
+python opencv_yolo.py --video assets/cars.mp4 --config darknet/yolov3.config --weight darknet/yolov3.weights --conf 0.5 --nms 0.4 --resol 416
+```
+
+#### FasterRCNN Pytorch
+```Shell
+python pytorch_pretrained.py --video assets/cars.mp4 --conf 0.5
+```
+
+#### Yolo Pytorch
+```Shell
+python pytorch_yolo.py --video assets/cars.mp4 --config darknet/yolov3.config --weight darknet/yolov3.weights --conf 0.5 --nms 0.4 --resolution 416
+```
+
+#### FasterRCNN Tensorflow
+```Shell
+python tensorflow_pretrained.py --video assets/cars.mp4 --frozen /path/to/frozen_inference_graph.pb --conf 0.5
+```
+
+#### Yolo Tensorflow
+```Shell
+python tensorflow_yolo.py --video assets/cars.mp4 --ckpt darknet/yolov3.ckpt --conf 0.5 --nms 0.4 --achor_path darknet/yolo_anchors.txt --resolution 416
+```
+<div align="center">
+<img src="readme/macpro.gif" width="600px"/>
+</div>
+<div style="display:inline-block;">
+<img src="readme/peopledesk.png" height="250px"/>
+<img src="readme/cars.png" height="250px"/>
+</div>
+
+
+## Running Demos on Webcam
+In order to run object detection on webcam, simply remove `--video` and add `--webcam True`. This is same to other programs as well.<br/>
+Example)  fasterrcnn pytorch
+```Shell
+python pytorch_pretrained.py --webcam True --conf 0.5
+```
+
+### Credits:
+Video by Pixabay from Pexels<br/>
+Video by clara blasco from Pexels <br/>
+Video by Pixly Videos from Pexels<br/>
+Video by Life of Pix from Pexels<br/>
+Video by George Morina from Pexels <br/>
---
+++ tensorflow_pretrained_multithreading.py
@@ -0,0 +1,189 @@
+from __future__ import division, print_function
+import numpy as np
+import os
+import tensorflow as tf
+import cv2
+import argparse
+import imutils
+
+from utils import ops as utils_ops, detection_boxes_tensorflow as vis
+from utils import label_map_util
+import time
+import sys
+
+
+from thread_w_return import *
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description='Tensorflow Pretrained')
+    parser.add_argument("--video", help="Path where video is located",
+                        default="assets/cars.mp4", type=str)
+    parser.add_argument("--frozen", help="Frozen inference pb file",
+                        default="faster_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb")
+    parser.add_argument("--conf", dest="confidence", help="Confidence threshold for predictions", default=0.5)
+    parser.add_argument("--save", help="Whether to save newly created vidoe", default=False, type=bool)
+    return parser.parse_args()
+
+
+def run_inference_for_single_image(image, tensor_dict, sess, detection_graph):
+    if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    out_dict = sess.run(
+        tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)}
+    )
+
+    # all outputs are float32 numpy arrays, so convert types as appropriate
+    out_dict['num_detections'] = int(out_dict['num_detections'][0])
+    out_dict['detection_classes'] = out_dict[
+        'detection_classes'][0].astype(np.uint8)
+    out_dict['detection_boxes'] = out_dict['detection_boxes'][0]
+    out_dict['detection_scores'] = out_dict['detection_scores'][0]
+    if 'detection_masks' in out_dict:
+        out_dict['detection_masks'] = out_dict['detection_masks'][0]
+    return out_dict
+
+
+def detection_gpu(frame_list, device_name, sess, tensor_dict, category_index, confidence, detection_graph):
+    frame_with_rect = []
+    with tf.device(device_name):
+        for frame in frame_list:
+            start = time.time()
+            output_dict = run_inference_for_single_image(frame, tensor_dict, sess, detection_graph)
+
+            # Visualization of the results of a detection.
+            vis.visualize_boxes_and_labels_rcnn(
+                frame,
+                output_dict['detection_boxes'],
+                output_dict['detection_classes'],
+                output_dict['detection_scores'],
+                category_index,
+                instance_masks=output_dict.get('detection_masks'),
+                use_normalized_coordinates=True,
+                min_score_thresh=confidence)
+            end = time.time()
+
+            cv2.putText(frame, '{:.2f}ms'.format((end - start) * 1000), (40, 40),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 0), 2)
+            frame_with_rect.append(frame)
+            cv2.imshow(device_name, frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    return frame_with_rect
+
+
+def main():
+    args = arg_parse()
+
+    VIDEO_PATH = args.video
+
+    # List of the strings that is used to add correct label for each box.
+    PATH_TO_LABELS = os.path.join('labels', 'mscoco_label_map.pbtxt')
+
+    # Load a Tensorflow model into memory
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.io.gfile.GFile(args.frozen, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+    category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+    try:
+        # Read Video file
+        cap = cv2.VideoCapture(VIDEO_PATH)
+    except IOError:
+        print("Input video file", VIDEO_PATH, "doesn't exist")
+        sys.exit(1)
+
+    if args.save:
+        video_width = int(cap.get(3))
+        video_height = int(cap.get(4))
+        video_fps = int(cap.get(5))
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        videoWriter = cv2.VideoWriter('video_result.mp4', fourcc, video_fps, (video_width, video_height))
+
+    # gpu가 몇개인지 알아낸다
+    gpus = tf.config.experimental.list_logical_devices('GPU')
+    frame_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # gpu개수에 맞춰서 frame을 나눈다
+    div = frame_length // len(gpus)
+    divide_point = [i for i in range(frame_length) if i != 0 and i % div == 0]
+    divide_point.pop()
+
+    frame_list = []
+    fragments = []
+    count = 0
+    while cap.isOpened():
+        hasFrame, frame = cap.read()
+        if not hasFrame:
+            frame_list.append(fragments)
+            break
+        if count in divide_point:
+            frame_list.append(fragments)
+            fragments = []
+        fragments.append(frame)
+        count += 1
+    cap.release()
+
+    with detection_graph.as_default():
+        with tf.Session() as sess:
+
+            # Get handles to input and output tensors
+            ops = tf.get_default_graph().get_operations()
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
+            tensor_dict = {}
+            for key in [
+                'num_detections', 'detection_boxes', 'detection_scores',
+                'detection_classes', 'detection_masks'
+            ]:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                        tensor_name)
+
+            # Threading을 통해서 여러 frame들을 detection하게 한다
+            thread_detection = [ThreadWithReturnValue(target=detection_gpu,
+                                                      args=(frame_list[i], gpu.name, sess, tensor_dict, category_index, args.confidence, detection_graph))
+                                for i, gpu in enumerate(gpus)]
+
+            final_list = []
+            # Threading 을 시작한다
+            for th in thread_detection:
+                th.start()
+
+            # Threading 이 끝나면 return 받은 값을 새로운 리스트에 담는다
+            for th in thread_detection:
+                final_list.extend(th.join())
+
+            # return 받은 value 를 video 에 작성한다
+            if args.save:
+                for f in final_list:
+                    videoWriter.write(f)
+
+
+if __name__ == "__main__":
+    s = time.time()
+    main()
+    e = time.time()
+    print((e-s))
+
+# tensorflow pretrained - 46.5966272354126
